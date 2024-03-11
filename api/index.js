e6e1183b33fd7cf99d3faf1a175740fd0fe85c74
NoinBlake336 <josechocobar@ipetym69.edu.ar>
2024-03-11 05:23:26 +0100

    preparations

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const {config} = require('./config');
+const {config} = require('./config/index');
 
 
 
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.get('/',
     async(req,res,next)=>{
-        res.send('Bienvenido a api MakeUp');
+        res.send('Welcome to api MakeUp');
     }
 );
 
