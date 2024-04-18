7df9a41dc7322e032ad38347725cffb7bbf77a35
NoinBlake336 <josechocobar336@gmail.com>
2024-04-18 12:51:42 -0300

    rename file

--- src/js/requests/fetchItemsRecommendations.js
+++ src/js/requests/fetchItemsRecommendations.js
@@ -1,8 +1,8 @@
 
 import { config } from "../config/index.config.js";
+import { sketchingRecommendedProducts } from "../drawingCard/SketchingRecommendedProducts.js";
 import { hiddenSkeleton } from "../skeleton-loaders/hiddenSkeleton.js";
 import { fetchRecommendationsProducts } from "./fetchProducts.js";
-import { sketchingRecommendedProducts } from "../drawingCard/SketchingRecommendedProducts.js";
 
 
 
